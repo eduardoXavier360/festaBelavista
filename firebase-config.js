e49034a894ad0dad143ff214f1f946dd47589716
eduardoXavier360 <eduardokungfu01@hotmail.com>
2025-06-01 20:19:46 -0300

    meu pai

--- firebase-config.js
+++ firebase-config.js
@@ -4,18 +4,19 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-firestore.js";
 
-// Configuração do Firebase do seu projeto
+// Configuração correta do Firebase
 const firebaseConfig = {
     apiKey: "AIzaSyBbnj2yINGp-40cMK5hw_gUkNj5on8NREI",
     authDomain: "belavista2025-1186d.firebaseapp.com",
     projectId: "belavista2025-1186d",
-    storageBucket: "belavista2025-1186d.firebasestorage.app",
+    storageBucket: "belavista2025-1186d.appspot.com", // CORRIGIDO
     messagingSenderId: "1012774404009",
     appId: "1:1012774404009:web:87874367783aab92bb8eb8",
     measurementId: "G-VF2N5H1KWP"
 };
+
 // Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
 
-// Exporta o Firestore para ser usado nos outros arquivos
+// Exporta o Firestore
 export const db = getFirestore(app);
